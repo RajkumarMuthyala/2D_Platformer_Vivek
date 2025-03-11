c62db674a2aad119fb15637b8c3b6797d42c9385
vivekkashyap17 <kashyap17vivekks@gmail.com>
2025-03-11 17:24:41 +0530

    Melee enemy movement completed

---
+++ GG Game1/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [Header ("Patrol Points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+
+    private void Awake()
+    {
+        initScale = transform.localScale;   
+    }
+
+    private void Update()
+    {
+        if (movingLeft)
+        {
+            if(enemy.position.x >= leftEdge.position.x)
+            MoveInDirection(-1);
+            else
+            {
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if(enemy.position.x <= leftEdge.position.x)
+            MoveInDirection(1);
+            else
+            {
+                DirectionChange();
+            }
+        }
+    }
+
+    private void DirectionChange()
+    {
+        movingLeft = !movingLeft;
+    }
+
+    private void MoveInDirection(int _direction)
+    {
+        //make enemy face direction
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction,
+            initScale.y, initScale.z);
+        //move in that position
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, 
+            enemy.position.y, enemy.position.z);
+    }
+
+
+}
